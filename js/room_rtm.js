5ded079199276b751d6879631384f0c52d082d98
ShubhamPal19 <shubhampal2309@gmail.com>
2023-06-02 13:01:32 +0530

    code pushed

---
+++ js/room_rtm.js
@@ -0,0 +1,155 @@
+
+let handleMemberJoined = async(MemberId)=>{
+
+    addMemberToDom(MemberId)
+    let  members = await channel.getMembers()
+    updateMemberTotal(members)
+    let {name } = await rtmClient.getUserAttributesByKeys(MemberId,['name'])
+   
+    addBotMessageToDom(`Welcome to the room ${name}! 👋`)
+
+}
+
+
+
+let addMemberToDom = async (MemberId)=>{
+    let {name } = await rtmClient.getUserAttributesByKeys(MemberId,['name'])
+    let membersWrapper = document.getElementById('member__list')
+    let memberItem = `<div class="member__wrapper" id="member__${MemberId}__wrapper">
+                            <span class="green__icon"></span>
+                            <p class="member_name">${name}</p>
+                      </div>`
+
+
+    membersWrapper.insertAdjacentHTML('beforeend', memberItem)
+
+}
+
+let updateMemberTotal = async (members) =>{
+
+    let total = document.getElementById('members__count')
+
+    total.innerText = members.length
+}
+
+let getMembers = async ()=>{
+
+    let members = await channel.getMembers()
+    updateMemberTotal(members)
+    for ( let i = 0 ; i<members.length ; i++ ){
+        addMemberToDom(members[i])
+    }
+
+}
+
+let handleMemberLeft = async(MemberId) =>{
+
+    removeMemberFromDom(MemberId)
+    let members = await channel.getMembers()
+    updateMemberTotal(members)
+}
+
+let removeMemberFromDom = async(MemberId) =>{
+    
+    let memberWrapper = document.getElementById(`member__${MemberId}__wrapper`)
+    let name = memberWrapper.getElementsByClassName('member_name')[0].textContent
+    addBotMessageToDom(`${name} has left the room`)
+    memberWrapper.remove();
+  
+
+
+
+}
+
+let handleChannelMessage = async (messageData, memberId) =>{
+
+   
+    let data = JSON.parse(messageData.text)
+    
+    if(data.type === 'chat'){
+
+        addMessageToDom(data.displayName,data.message)
+    }
+    if(data.type === 'user-left'){
+       
+        document.getElementById(`user-container-${data.UserId}`).remove()
+        if(userIdInDisplayFrame == `user-container-${data.UserId}`){
+
+            displayFrame.style.display = null
+    
+            for(let i = 0 ; videoFrame.length> i ; i++){
+                videoFrames[i].style.height = '300px'
+                videoFrames[i].style.width = '300px'
+            }
+        }
+    
+
+    }
+}
+let sendMessage = async (e) =>{
+
+    e.preventDefault();
+
+    let message = e.target.message.value
+
+    channel.sendMessage({text:JSON.stringify({'type':'chat', 'message':message,'displayName':displayName})})
+    addMessageToDom(displayName, message)
+    document.getElementById("message__form").reset();
+    e.target.reset();
+
+}
+
+let addMessageToDom = (name, message)=>{
+    let messageWrapper = document.getElementById('messages')
+    
+    let messageItem  = ` <div class="message__wrapper">
+                            <div class="message__body">
+                                <strong class="message__author">${name}</strong>
+                                <p class="message__text">${message}</p>
+                            </div>
+                        </div>`
+
+    messageWrapper.insertAdjacentHTML('beforeend',messageItem);
+
+    let lastMessage = document.querySelector('#messages .message__wrapper:last-child')
+
+    if(lastMessage){
+        lastMessage.scrollIntoView()
+    }
+}
+
+let addBotMessageToDom = (botMessage)=>{
+    let messageWrapper = document.getElementById('messages')
+    
+    let messageItem  = `<div class="message__wrapper">
+                            <div class="message__body__bot">
+                                <strong class="message__author__bot">🤖 Mumble Bot</strong>
+                                <p class="message__text__bot">${botMessage}</p>
+                            </div>
+                        </div>`
+
+    messageWrapper.insertAdjacentHTML('beforeend',messageItem);
+
+    let lastMessage = document.querySelector('#messages .message__wrapper:last-child')
+
+  
+    if(lastMessage){
+        lastMessage.scrollIntoView()
+    }
+}
+
+let leaveChannel = async(MemberId) =>
+{
+    addBotMessageToDom(`${displayName} has left`)
+
+    
+    await channel.leave();
+    await rtmClient.logout();
+}
+
+
+
+window.addEventListener('beforeunload',leaveChannel)
+
+let messageForm = document.getElementById('message__form')
+messageForm.addEventListener('submit', sendMessage)
